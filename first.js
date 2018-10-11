6d21951a41215b55712e25ab53dbab88d8f7f24b
Sreekanth Surampally <34951779+ssurampally@users.noreply.github.com>
2018-10-11 14:25:28 -0400

    Add files via upload

---
+++ first.js
@@ -0,0 +1,36 @@
+// Working with simple variable declaration with prompt
+
+/* function sayhello()
+   {
+    var text = prompt("you name please");
+    alert("hello" + text + "there");
+
+   }
+
+    sayhello() ;
+*/
+
+//working with functions with parameters
+/*
+function multiply(a,b) 
+ {    
+   return a * b ;
+ } ;
+
+console.log(multiply(6,10)) ; 
+console.log(multiply(20,20));
+*/
+
+// working with Array, multiple variables
+/*
+var customers = ['BestBuy', 'Walmart', 'Costco'];
+console.log(customers);
+customers.push('HomeDepot'); // to add the list of array
+customers.pop() ;  // to remove the last item in the array
+console.log(customers);
+*/
+
+// Working with Loops
+
+
+
